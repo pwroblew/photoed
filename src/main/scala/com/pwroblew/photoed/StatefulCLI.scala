8a3d63710a1be8ef3979d9bebf4dfdc5d5926c49
Paweł Wróblewski <8215613+pwroblew@users.noreply.github.com>
2026-07-15 18:40:52 +0200

    2026-07-15 - 02 - changed the way how resources are used: allocated instead of use

--- src/main/scala/com/pwroblew/photoed/StatefulCLI.scala
+++ src/main/scala/com/pwroblew/photoed/StatefulCLI.scala
@@ -1,16 +1,10 @@
 package com.pwroblew.photoed
 
-import cats.effect.{IO, IOApp, Ref, Resource}
+import cats.effect.{IO, IOApp, Ref}
 import cats.implicits.catsSyntaxOptionId
+import com.pwroblew.photoed.lib.*
 import com.pwroblew.photoed.lib.impl_f.PhotoEdAppImpl
 import com.pwroblew.photoed.lib.impl_io.{EdImageFilesImpl, EdImageViewerImpl}
-import com.pwroblew.photoed.lib.{
-  EdImageViewer,
-  PhotoEdAppState,
-  TO_BE_CONTINUED,
-  TO_BE_CONTINUED_BUT_NOT_SHOWN,
-  TO_BE_SHOWN
-}
 
 object StatefulCLI extends IOApp.Simple {
 
@@ -25,9 +19,12 @@ object StatefulCLI extends IOApp.Simple {
     for {
       _         <- nextAppStep(appState, None).whileM_(TO_BE_CONTINUED_BUT_NOT_SHOWN(appState))
       toBeShown <- appState.get.map(_.toBeShown)
-      _         <- if (toBeShown) EdImageViewerImpl.makeResource("photoed").use { viewer =>
-                     nextAppStep(appState, viewer.some).whileM_(TO_BE_SHOWN(appState))
-                   }
+      _         <- if (toBeShown)
+                     for {
+                       (viewer, finalizer) <- EdImageViewerImpl.makeResource("photoed").allocated
+                       _                   <- nextAppStep(appState, viewer.some).whileM_(TO_BE_SHOWN(appState))
+                       _                   <- finalizer
+                     } yield ()
                    else IO.unit
     } yield ()
 
